ccb16fe1983f8a750c5573e77eeb4aa4c18dd537
Mo-Duoha <132151920+Mo-Duoha@users.noreply.github.com>
2024-12-15 14:36:41 +0200

    Add files via upload

---
+++ TicketManager.fs
@@ -0,0 +1,10 @@
+﻿module TicketManager
+open System
+
+type Ticket = { TicketID: string; CustomerName: string; Seat: string; Showtime: string }
+
+type TicketManager() =
+    member this.GenerateTicketID() = Guid.NewGuid().ToString()
+    member this.SaveTicket(ticket: Ticket) =
+        let ticketDetails = $"Ticket ID: {ticket.TicketID}\nCustomer: {ticket.CustomerName}\nSeat: {ticket.Seat}\nShowtime: {ticket.Showtime}\n\n"
+        System.IO.File.AppendAllText("bookings.txt", ticketDetails)
